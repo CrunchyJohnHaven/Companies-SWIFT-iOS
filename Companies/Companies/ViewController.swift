b63407c978e949b73aa34d4e98f7ca66c52ea1e2
John Bradley <johnhavenbradley@gmail.com>
2018-03-19 20:23:37 -0700

    update

--- Companies/Companies/ViewController.swift
+++ Companies/Companies/ViewController.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OneVC: UIViewController {
 
+    @IBAction func addAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "SegueOne", sender: self)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
+extension OneVC: UIViewController
 
